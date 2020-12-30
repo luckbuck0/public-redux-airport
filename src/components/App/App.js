7dfdc4eece93d67b406539470509a1d2975eb7de
Kris Szafranski <kris@thoughtshelter.com>
2020-12-30 10:21:33 -0600

    converted to function component, updated versions

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Redux Airport</h1>
-        <input placeholder="Airline Name" />
-        <button>Add Airline</button>
-        <table>
-          {/* Airlines should be listed here */}
-        </table>
-      </div>
-    );
-  }
+function App() {
+
+  return (
+    <div>
+      <h1>Redux Airport</h1>
+      <input placeholder='Airline Name' />
+      <button>Add Airline</button>
+      <table>{/* Airlines should be listed here */}</table>
+    </div>
+  );
 }
 
 export default App;
